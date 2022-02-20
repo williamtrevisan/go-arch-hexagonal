21b9e6df2e11514e3960f03c37eea6161337a84b
William Trevisan <williamtrev@hotmail.com>
2022-02-20 11:59:00 -0300

    Implementing mock for tests, and create tests for product service

---
+++ app/product_service_test.go
@@ -0,0 +1,63 @@
+package app_test
+
+import (
+    "github.com/golang/mock/gomock"
+    "github.com/stretchr/testify/require"
+    "github.com/williamtrevisan/go-arch-hexagonal/app"
+    mock_app "github.com/williamtrevisan/go-arch-hexagonal/app/mocks"
+    "testing"
+)
+
+func TestProductService_Create(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    product := mock_app.NewMockProductInterface(ctrl)
+
+    persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
+    persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+    service := app.ProductService{Persistence: persistence}
+
+    result, err := service.Create("Product", 10)
+    require.Nil(t, err)
+    require.Equal(t, product, result)
+}
+
+func TestProductService_Get(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    product := mock_app.NewMockProductInterface(ctrl)
+
+    persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
+    persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+    service := app.ProductService{Persistence: persistence}
+
+    result, err := service.Get("abc")
+    require.Nil(t, err)
+    require.Equal(t, product, result)
+}
+
+func TestProductService_EnableDisable(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    product := mock_app.NewMockProductInterface(ctrl)
+    product.EXPECT().Enable().Return(nil)
+    product.EXPECT().Disable().Return(nil)
+
+    persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
+    persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+    service := app.ProductService{Persistence: persistence}
+
+    result, err := service.Enable(product)
+    require.Nil(t, err)
+    require.Equal(t, product, result)
+
+    result, err = service.Disable(product)
+    require.Nil(t, err)
+    require.Equal(t, product, result)
+}
